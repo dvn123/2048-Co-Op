eb6c785901928d17b75a9807b444b0e7791627ef
dvn123 <diogovaznunes@gmail.com>
2014-06-20 15:03:10 +0100

    Fixed Indent

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ var game_state;
 var democracy_votes = 0;
 var anarchy_votes = 1;
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log(socket.user.id + ' user connected');
-    socket.on('move', function(direction, grid) {
-        if(game_state.grid == grid) {
+    socket.on('move', function (direction, grid) {
+        if (game_state.grid == grid) {
             io.emit('move', direction);
         }
     });
