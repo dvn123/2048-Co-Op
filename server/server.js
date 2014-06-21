f6a5c9593a02e93ef2b6e0b14308a533e1de4deb
dvn123 <diogovaznunes@gmail.com>
2014-06-21 18:52:11 +0100

    asd

--- server/server.js
+++ server/server.js
@@ -1,22 +1,6 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
+var io = require('socket.io')(80);
 var log4js = require('log4js');
 app.listen(80);
-
-
-function handler (req, res) {
-  fs.readFile(__dirname + '../index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
 
 
 log4js.clearAppenders();
